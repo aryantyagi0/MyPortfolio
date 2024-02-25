982723812f019a281e9c2e3f25e92c3534220765
aryan <at9120140@gmail.com>
2024-02-25 21:25:41 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// burger=document.querySelector('.burger');
+// navbar=document.querySelector('.navbar');
+// burger.addEventListener('click',()=>{
+//     navbar.classList.toggle('v-class');
+// })
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navlist=document.querySelector('.nav-list')
+rightnav=document.querySelector('.right-nav')
+burger.addEventListener('click',()=>{
+    rightnav.classList.toggle('v-class');
+    navlist.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav')
+
+
+})
